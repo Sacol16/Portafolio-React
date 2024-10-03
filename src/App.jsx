437507ff8c2ccf8f73cd3cd39f3d665472e62dac
Sacol16 <santiago.colonia@uao.edu.co>
2024-10-03 12:15:54 -0500

    Se creo el componente projects con 2 carruseles para mostrar los repositorios del primer y segundo corte

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from "./Components/Navbar";
 import Home from './Components/Home';
 import Skills from './Components/Skills';
+import Projects from './Components/Projects';
 import "./App.css";
 
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navigation />
       <Home />
       <Skills />
+      <Projects />
     </div>
   );
 };
