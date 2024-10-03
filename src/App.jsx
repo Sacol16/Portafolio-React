ef13acedf99def70f82caf011bd81678407e7dc9
Sacol16 <santiago.colonia@uao.edu.co>
2024-10-03 12:00:29 -0500

    Agregue una barra de navegación responsive con enlaces a las secciones de Home, Skills, Social Media y Projects utilizando React-Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "./Components/Navbar";
+
+import "./App.css";
+
+
+const App = () => {
+  return (
+    <div className='body'>
+      <Navigation />
+      
+    </div>
+  );
+};
+
+export default App;
